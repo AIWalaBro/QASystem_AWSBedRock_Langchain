0257955674c3a6c9a32f49879957d77fba06f615
AIWalaBro <aiwalabro@gmail.com>
2024-06-22 22:08:28 +0530

    setup local packages

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
+
+
 from setuptools import find_packages, setup
-setup()
+
+setup(
+    name="qasystem",
+    version="0.0.2",
+    author="bharatbhushan",
+    author_email="bharat.genai@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+    
